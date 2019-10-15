243516d9a7e51dd3c2cf9160cfe894fab9a33d24
l5x <189198+l5x@users.noreply.github.com>
2019-10-15 23:51:21 +0400

    fix: delimiter style

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ module.exports = {
     'plugin:prettier/recommended'
   ],
   rules: {
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false
+        }
+      }
+    ],
     indent: 'off',
     '@typescript-eslint/indent': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
