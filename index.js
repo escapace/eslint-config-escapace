999d69b481d4e5725b5d2472bbe9ad3ecb384040
yyxi <178086254+yyxi@users.noreply.github.com>
2020-12-13 10:38:42 +0400

    feat: disable rules that conflict with prettier

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ module.exports = {
   plugins: ['@typescript-eslint', 'no-null'],
   extends: [
     'standard-with-typescript',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   rules: {
     'no-void': 0,
