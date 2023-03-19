2b591ff223030c562350cb6e3c0c9e07ff101f45
yyxi <178086254+yyxi@users.noreply.github.com>
2023-03-19 16:42:19 +0100

    feat: use pnpm

--- install-peer-dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export PKG=eslint-config-escapace;
-npm info "$PKG@latest" peerDependencies --json | jq -r 'to_entries[] | "\"\(.key)@\(.value)\"" // empty' | xargs npm install --save-dev "$PKG@latest"
