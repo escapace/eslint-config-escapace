9e86c12cae18fa3966d55a1a2192608c478aa21d
yyxi <178086254+yyxi@users.noreply.github.com>
2025-03-05 18:07:04 +0000

    fix: test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -17,8 +17,8 @@ import {
 } from './config'
 import type { RuleEntry } from './types'
 import { ruleDefinitions } from './utilities/rule-definitions'
-// import { promisify } from 'node:util'
-// const exec = promisify(_exec)
+import { promisify } from 'node:util'
+const exec = promisify(_exec)
 
 const rulesAll = (await ruleDefinitions()).map(([key]) => key)
 
@@ -53,6 +53,6 @@ it('rules', () => {
   checks(rulesVueIncluded, rulesVueDefaults)
 })
 
-// it('eslint', { timeout: 30_000 }, async () => {
-//   await exec('./node_modules/.bin/eslint')
-// })
+it('eslint', { timeout: 30_000 }, async () => {
+  await exec(`./node_modules/.bin/eslint 'src/**/*.ts'`)
+})
