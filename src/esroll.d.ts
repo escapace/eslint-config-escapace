fcbd910a3a3178722978695ac9f9ed6bb4c69a47
yyxi <178086254+yyxi@users.noreply.github.com>
2026-01-08 18:59:19 +0000

    fix: updates

--- src/esroll.d.ts
+++ src/esroll.d.ts
@@ -1,6 +1,6 @@
 declare global {
   const __ENVIRONMENT__: 'production' | 'staging' | 'testing' | undefined
-  const __PLATFORM__: 'browser' | 'node' | undefined
+  const __PLATFORM__: 'node' | undefined
   const __VERSION__: string | undefined
 }
 
